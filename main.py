89c31f543b701261235231150a5457d73e9d467c
Kate Biliak <kate.bilyak@gmail.com>
2022-10-12 16:42:14 -0400

    Added implementation for working with functions and modules

---
+++ main.py
@@ -0,0 +1,12 @@
+import my_lib 
+
+def main():
+	func_1_result = my_lib.func_1()
+	func_2_result = my_lib.func_2()
+	func_3_result = my_lib.func_3()
+	print(func_1_result) # must be 1
+	print(func_2_result) # must be 2
+	print(func_3_result) # must be 3
+
+	
+main()
---
+++ my_lib.py
@@ -0,0 +1,9 @@
+def func_1():
+    return 5
+
+def func_2():
+    return 2
+
+def func_3():
+    return 3
+
